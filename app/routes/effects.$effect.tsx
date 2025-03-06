0e5b6b25d62b8917f7e4d4419b453329ad4cf141
sBoudacherCLIQ <s.boudacher@cliqdigital.com>
2025-03-06 14:03:23 +0100

    test prod no edge function

--- app/routes/effects.$effect.tsx
+++ app/routes/effects.$effect.tsx
@@ -1,19 +1,37 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData, useParams } from "@remix-run/react";
-import { EffectChoice, getEffect } from "~/api/effects/getEffect";
+import prisma from "~/lib/prismaDb";
+
+// export const loader = async ({ params }: LoaderFunctionArgs) => {
+//   const effects = await getEffect(params.effect as EffectChoice);
+//   if (!effects) {
+//     throw new Response("Not Found", { status: 404 });
+//   }
+//   console.log(effects);
+//   return { effects };
+// };
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const effects = await getEffect(params.effect as EffectChoice);
-  if (!effects) {
+  const bodyEffects = await prisma.bodyEffect.findMany({
+    select: {
+      id: true,
+      effect: true,
+    },
+    orderBy: {
+      effect: "asc",
+    },
+  });
+
+  if (!bodyEffects) {
     throw new Response("Not Found", { status: 404 });
   }
-  console.log(effects);
-  return { effects };
+
+  return bodyEffects;
 };
 
 export default function Effect() {
   const params = useParams();
-  const { effects } = useLoaderData<typeof loader>();
+  const bodyEffects = useLoaderData<typeof loader>();
 
   return (
     <div className="flex flex-col gap-4">
@@ -22,7 +40,7 @@ export default function Effect() {
         <span className="font-bold"> {params.effect}</span>.
       </p>
       <div className="flex flex-col gap-4">
-        {effects.map((effect) => (
+        {bodyEffects.map((effect) => (
           <p key={effect.id}>{effect.effect}</p>
         ))}
       </div>
