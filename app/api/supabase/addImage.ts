db9dad5d5c4709857a5d17d4a167bccc4d3747a9
sBoudacherCLIQ <s.boudacher@cliqdigital.com>
2025-03-10 18:29:24 +0100

    feat(stones): add image upload functionality to stone creation form

--- app/api/supabase/addImage.ts
+++ app/api/supabase/addImage.ts
@@ -1,11 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
-const supabase = createClient(supabaseUrl, supabaseKey);
+import supabaseClient from "~/lib/supabase";
 
 export async function addImageToBucket(file: File) {
-  const { data, error } = await supabase.storage
+  const { data, error } = await supabaseClient()
+    .storage
     .from("lithoRemixBucket")
     .upload(`${file.name}`, file);
 if (error) {
@@ -15,4 +12,3 @@ if (error) {
 }
 return data;
 }
-
--- app/routes/stones.create.tsx
+++ app/routes/stones.create.tsx
@@ -1,10 +1,4 @@
-import {
-  json,
-  redirect,
-  useFetcher,
-  useLoaderData,
-  useParams,
-} from "@remix-run/react";
+import { json, redirect, useFetcher, useLoaderData } from "@remix-run/react";
 import { getBodyEffects } from "~/api/effects/bodyEffects/getBodyEffects";
 import { getEmotionalEffect } from "~/api/effects/emotionalEffect/getEmotionalEffect";
 import { getSpiritualEffect } from "~/api/effects/spiritualEffect/getSpiritualEffect";
@@ -16,6 +10,7 @@ import { getRechargementTypes } from "~/api/types/rechargement/getRechargementTy
 
 import { Select, SelectItem } from "@heroui/select";
 import { ActionFunctionArgs } from "@remix-run/node";
+import { useState } from "react";
 import { createStone } from "~/api/stones/createStone";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
@@ -96,7 +91,6 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 export default function StonesCreate() {
-  const params = useParams();
   const fetcher = useFetcher();
   const {
     bodyEffects,
@@ -109,154 +103,180 @@ export default function StonesCreate() {
     contraindications,
   } = useLoaderData<typeof loader>();
 
-  return (
-    <fetcher.Form method="post" id="create-stone-form">
-      <div className="flex flex-col gap-6 mt-10">
-        <div className="flex flex-row gap-6">
-          <div className="flex flex-col gap-6 w-full max-w-2xl">
-            <div>
-              <p className="text-md font-semibold text-zinc-800 mb-3">
-                Nom de la pierre
-              </p>
-              <Input
-                name="name"
-                placeholder="Entrez le nom de la pierre"
-                className="max-w-xs"
-              />
-            </div>
+  const [image, setImage] = useState<File | null>(null);
 
-            <div className="flex flex-row gap-6 w-full justify-start">
-              <div className="w-full max-w-xs">
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImage(file);
+      console.log(file);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-6 mt-10">
+        <Input
+          name="image"
+          placeholder="Image"
+          className="max-w-xs"
+          accept="image/*"
+          type="file"
+          onChange={handleImageChange}
+        />
+      </div>
+      <fetcher.Form method="post" id="create-stone-form">
+        <div className="flex flex-col gap-6 mt-10">
+          <div className="flex flex-row gap-6">
+            <div className="flex flex-col gap-6 w-full max-w-2xl">
+              <div>
                 <p className="text-md font-semibold text-zinc-800 mb-3">
-                  Description
+                  Nom de la pierre
                 </p>
-                <Textarea
-                  name="description"
-                  placeholder="Entrez la description de la pierre"
+                <Input
+                  name="name"
+                  placeholder="Entrez le nom de la pierre"
+                  className="max-w-xs"
                 />
+              </div>
+
+              <div className="flex flex-row gap-6 w-full justify-start">
+                <div className="w-full max-w-xs">
+                  <p className="text-md font-semibold text-zinc-800 mb-3">
+                    Description
+                  </p>
+                  <Textarea
+                    name="description"
+                    placeholder="Entrez la description de la pierre"
+                  />
+                </div>
               </div>
             </div>
           </div>
+
+          <div className="flex flex-row gap-6">
+            <div className="flex flex-col gap-4 w-full max-w-xs">
+              <p className="text-md font-semibold text-zinc-800">Effets</p>
+              <Select
+                className="max-w-xs"
+                label="Corporel"
+                name="bodyEffects"
+                placeholder="Sélectionnez un effet corporel"
+                selectionMode="multiple"
+              >
+                {bodyEffects.map((bodyEffect) => (
+                  <SelectItem key={bodyEffect.id}>
+                    {bodyEffect.effect}
+                  </SelectItem>
+                ))}
+              </Select>
+
+              <Select
+                className="max-w-xs"
+                label="Emotionnel"
+                name="emotionalEffects"
+                placeholder="Sélectionnez un effet emotionnel"
+                selectionMode="multiple"
+              >
+                {emotionalEffects.map((emotionalEffect) => (
+                  <SelectItem key={emotionalEffect.id}>
+                    {emotionalEffect.effect}
+                  </SelectItem>
+                ))}
+              </Select>
+
+              <Select
+                className="max-w-xs"
+                label="Spirituel"
+                name="spiritualEffects"
+                placeholder="Sélectionnez un effet spirituel"
+                selectionMode="multiple"
+              >
+                {spiritualEffects.map((spiritualEffect) => (
+                  <SelectItem key={spiritualEffect.id}>
+                    {spiritualEffect.effect}
+                  </SelectItem>
+                ))}
+              </Select>
+            </div>
+            <div className="flex flex-col gap-4 w-full max-w-xs">
+              <p className="text-md font-semibold text-zinc-800">Types</p>
+              <Select
+                className="max-w-xs"
+                label="Rechargement"
+                name="rechargementTypes"
+                placeholder="Sélectionnez un type de rechargement"
+                selectionMode="multiple"
+              >
+                {rechargementTypes.map((rechargementType) => (
+                  <SelectItem key={rechargementType.id}>
+                    {rechargementType.type}
+                  </SelectItem>
+                ))}
+              </Select>
+
+              <Select
+                className="max-w-xs"
+                label="Purification"
+                name="purificationTypes"
+                placeholder="Sélectionnez un type de purification"
+                selectionMode="multiple"
+              >
+                {purificationTypes.map((purificationType) => (
+                  <SelectItem key={purificationType.id}>
+                    {purificationType.type}
+                  </SelectItem>
+                ))}
+              </Select>
+            </div>
+            <div className="flex flex-col gap-4 w-full max-w-xs">
+              <p className="text-md font-semibold text-zinc-800">
+                Autres paramètres
+              </p>
+              <Select
+                className="max-w-xs"
+                label="Forme artisanale"
+                name="craftedForms"
+                placeholder="Sélectionnez une forme artisanale"
+                selectionMode="multiple"
+              >
+                {craftedForms.map((craftedForm) => (
+                  <SelectItem key={craftedForm.id}>
+                    {craftedForm.form}
+                  </SelectItem>
+                ))}
+              </Select>
+
+              <Select
+                className="max-w-xs"
+                label="Chakra"
+                name="chakras"
+                placeholder="Sélectionnez un chakra"
+                selectionMode="multiple"
+              >
+                {chakras.map((chakra) => (
+                  <SelectItem key={chakra.id}>{chakra.number}</SelectItem>
+                ))}
+              </Select>
+
+              <Select
+                className="max-w-xs"
+                label="Contre-indications"
+                name="contraindications"
+                placeholder="Sélectionnez une contre-indication"
+              >
+                {contraindications.map((contraindication) => (
+                  <SelectItem key={contraindication.id}>
+                    {contraindication.contraindicationName}
+                  </SelectItem>
+                ))}
+              </Select>
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-row gap-6">
-          <div className="flex flex-col gap-4 w-full max-w-xs">
-            <p className="text-md font-semibold text-zinc-800">Effets</p>
-            <Select
-              className="max-w-xs"
-              label="Corporel"
-              name="bodyEffects"
-              placeholder="Sélectionnez un effet corporel"
-              selectionMode="multiple"
-            >
-              {bodyEffects.map((bodyEffect) => (
-                <SelectItem key={bodyEffect.id}>{bodyEffect.effect}</SelectItem>
-              ))}
-            </Select>
-
-            <Select
-              className="max-w-xs"
-              label="Emotionnel"
-              name="emotionalEffects"
-              placeholder="Sélectionnez un effet emotionnel"
-              selectionMode="multiple"
-            >
-              {emotionalEffects.map((emotionalEffect) => (
-                <SelectItem key={emotionalEffect.id}>
-                  {emotionalEffect.effect}
-                </SelectItem>
-              ))}
-            </Select>
-
-            <Select
-              className="max-w-xs"
-              label="Spirituel"
-              name="spiritualEffects"
-              placeholder="Sélectionnez un effet spirituel"
-              selectionMode="multiple"
-            >
-              {spiritualEffects.map((spiritualEffect) => (
-                <SelectItem key={spiritualEffect.id}>
-                  {spiritualEffect.effect}
-                </SelectItem>
-              ))}
-            </Select>
-          </div>
-          <div className="flex flex-col gap-4 w-full max-w-xs">
-            <p className="text-md font-semibold text-zinc-800">Types</p>
-            <Select
-              className="max-w-xs"
-              label="Rechargement"
-              name="rechargementTypes"
-              placeholder="Sélectionnez un type de rechargement"
-              selectionMode="multiple"
-            >
-              {rechargementTypes.map((rechargementType) => (
-                <SelectItem key={rechargementType.id}>
-                  {rechargementType.type}
-                </SelectItem>
-              ))}
-            </Select>
-
-            <Select
-              className="max-w-xs"
-              label="Purification"
-              name="purificationTypes"
-              placeholder="Sélectionnez un type de purification"
-              selectionMode="multiple"
-            >
-              {purificationTypes.map((purificationType) => (
-                <SelectItem key={purificationType.id}>
-                  {purificationType.type}
-                </SelectItem>
-              ))}
-            </Select>
-          </div>
-          <div className="flex flex-col gap-4 w-full max-w-xs">
-            <p className="text-md font-semibold text-zinc-800">
-              Autres paramètres
-            </p>
-            <Select
-              className="max-w-xs"
-              label="Forme artisanale"
-              name="craftedForms"
-              placeholder="Sélectionnez une forme artisanale"
-              selectionMode="multiple"
-            >
-              {craftedForms.map((craftedForm) => (
-                <SelectItem key={craftedForm.id}>{craftedForm.form}</SelectItem>
-              ))}
-            </Select>
-
-            <Select
-              className="max-w-xs"
-              label="Chakra"
-              name="chakras"
-              placeholder="Sélectionnez un chakra"
-              selectionMode="multiple"
-            >
-              {chakras.map((chakra) => (
-                <SelectItem key={chakra.id}>{chakra.number}</SelectItem>
-              ))}
-            </Select>
-
-            <Select
-              className="max-w-xs"
-              label="Contre-indications"
-              name="contraindications"
-              placeholder="Sélectionnez une contre-indication"
-            >
-              {contraindications.map((contraindication) => (
-                <SelectItem key={contraindication.id}>
-                  {contraindication.contraindicationName}
-                </SelectItem>
-              ))}
-            </Select>
-          </div>
-        </div>
-      </div>
-
-      <Button className="w-fit mt-10">Ajouter une pierre</Button>
-    </fetcher.Form>
+        <Button className="w-fit mt-10">Ajouter une pierre</Button>
+      </fetcher.Form>
+    </>
   );
 }
